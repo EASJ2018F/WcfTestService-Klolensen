8b8d2430ede3315abfa68166ff89f1b683d5710d
Klolensen <31515047+Klolensen@users.noreply.github.com>
2018-02-12 10:39:19 +0100

    Fixed it

---
+++ WcfServiceTest/ServiceTest.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceTest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceTest" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceTest.svc or ServiceTest.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceTest : ITestService
+    {
+        public string GetMyName()
+        {
+            return "Kevin Jensen";
+        }
+    }
+}
